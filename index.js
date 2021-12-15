313e5fe76108bed640ab18cda3e76b886250baf7
jonathan bola <bolajonathan@yahoo.com>
2021-12-15 16:55:49 -0600

    commit tree 8c0e8738b5615e53b4bd0f7b736dbfc7e13b056f

---
+++ index.js
@@ -0,0 +1,19 @@
+function receivesAFunction(callback){
+
+    callback();
+
+}
+
+function returnsANamedFunction (){
+
+return function myNamedFunction(){
+
+}
+
+}
+function returnsAnAnonymousFunction  (){
+    return function(){
+        
+    };
+}
+
